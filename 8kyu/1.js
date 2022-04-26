97cef7104846f53547fc4ba3274839eb2c51ffdf
AsTiL <AsTiL@DESKTOP-M9P1BV5>
2022-04-26 13:26:24 +0300

    refactor: change structure of the project

--- 8kyu/1.js
+++ 8kyu/1.js
@@ -2,7 +2,7 @@
 // Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
 
 function digitize(n) {
-    return n.toString().split('').map(el => Number(el)).reverse();
+    return String(n).split('').map(Number).reverse();
 }
 
 console.log(digitize(12345))
