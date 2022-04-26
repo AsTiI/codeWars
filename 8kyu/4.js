9c3f7e8a23f3a47806e98306501f20f893631413
AsTiL <AsTiL@DESKTOP-M9P1BV5>
2022-04-26 17:02:05 +0300

    feat: create function witch return respective ages

--- 8kyu/4.js
+++ 8kyu/4.js
@@ -18,3 +18,31 @@
 // +9 dog years for second year
 // +5 dog years for each year after that
 
+var humanYearsCatYearsDogYears = function(humanYears) {
+    let a = [0,0,0]
+    console.log(a)
+    for(let i=1; i<humanYears+1; i++){
+        for(let j=0; j<3; j++){
+            if(i==1)
+                switch(j){
+                    case 0: a[j]++;break;
+                    case 1: a[j]+=15;break;
+                    case 2: a[j]+=15;break;
+                }
+            if(i==2)
+                switch(j){
+                    case 0: a[j]++;break;
+                    case 1: a[j]+=9;break;
+                    case 2: a[j]+=9;break;
+                }
+            if(i>2)
+                switch(j){
+                    case 0: a[j]++;break;
+                    case 1: a[j]+=4;break;
+                    case 2: a[j]+=5;break;
+                }
+        }
+    }
+    console.log(a)
+    return a;
+}
