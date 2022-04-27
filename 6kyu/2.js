64b8a49e2bdc13d6d6edf24776acf1fc06378371
AsTiL <AsTiL@DESKTOP-M9P1BV5>
2022-04-27 16:17:23 +0300

    feat: create diamond which return a strhing that looks like a diamond shape

--- 6kyu/2.js
+++ 6kyu/2.js
@@ -5,3 +5,51 @@
 // Return null/nil/None/... if the input is an even number or negative, as it is not
 // possible to print a diamond of even or negative size.
 
+function diamond(size){
+    let p = ""
+    if (size > 1 && size % 2 !== 0){
+        let d = 1
+        let b = ((size+1)/2)-1
+        for(let i=0;i<((size+1)/2);i++){
+
+            if (b !== 0){
+                for(let j=0;j<b;j++){
+                    p += " "
+                }
+                b -= 1
+                for(let m=0;m<d;m++){
+                    p += "*"
+                }
+                d += 2
+
+                p += "\n"
+            }
+            else{
+                for(let n=0;n<d;n++){
+                    p += "*"
+                }
+                p += "\n"
+            }
+
+        }
+        b += 1
+        d -= 2
+        for (let i=0;i<((size+1)/2-1);i++){
+            for(let j=0;j<b;j++){
+                p += " "
+            }
+            b += 1
+            for(let m=0;m<d;m++){
+                p += "*"
+            }
+            d -= 2
+            p += "\n"
+        }
+        return p
+    }else if (size === 1){
+        return "*\n"
+    }
+    return null
+}
+
+console.log(diamond(5));
