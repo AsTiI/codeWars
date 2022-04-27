468c71c686c819cca04843d14da405e6a7b7d319
AsTiL <AsTiL@DESKTOP-M9P1BV5>
2022-04-27 15:15:50 +0300

    feat: create isValidIp which identify valid IPv4

--- 6kyu/1.js
+++ 6kyu/1.js
@@ -2,3 +2,8 @@
 // IPs should be considered valid if they consist of four octets, with values between
 // 0 and 255, inclusive.
 
+function isValidIP(str) {
+    return /^(25[0-5]|2[0-4][0-9]|[1][0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|[1][0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|[1][0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|[1][0-9][0-9]|[1-9]?[0-9])$/.test(str);
+}
+
+console.log(isValidIP('1.2.123.123'))
