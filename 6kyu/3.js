9a863d4e0bbec0dab84a740528b8acbf4b68f1c1
AsTiL <AsTiL@DESKTOP-M9P1BV5>
2022-04-27 16:35:27 +0300

    feat: create solution which return the sum of all the multiples of 3 or 5 below

--- 6kyu/3.js
+++ 6kyu/3.js
@@ -4,3 +4,15 @@
 // Finish the solution so that it returns the sum of all the multiples of 3 or 5 below
 // the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
 
+function solution(number){
+    if(number<=0)
+        return 0;
+    let sum = 0;
+    for(let i = 1; i < number; i++){
+        if(i % 3 === 0 || i % 5 === 0)
+            sum += i;
+    }
+    return sum;
+}
+
+console.log(solution(10,23))
