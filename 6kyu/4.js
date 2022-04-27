992fd7585b9909ea8f51c3e32f61ec2392246478
AsTiL <AsTiL@DESKTOP-M9P1BV5>
2022-04-27 17:15:13 +0300

    feat: create solution which takes a Roman numeral as its argument and returns its value as a numeric deimal integer

--- 6kyu/4.js
+++ 6kyu/4.js
@@ -8,3 +8,25 @@
 // is rendered "MMVIII" (2000 = MM, 8 = VIII). The Roman numeral for 1666,
 // "MDCLXVI", uses each letter in descending order.
 
+function solution (roman) {
+    let romanMap = new Map([
+        ['I',1],
+        ['V',5],
+        ['X',10],
+        ['L',50],
+        ['C',100],
+        ['D',500],
+        ['M',1000]
+    ])
+    let arr = roman.split('').map(el=>{
+        for(let key of romanMap.keys())
+            if(el===key)
+                return romanMap.get(key)
+    })
+    let sum =0;
+    for(let i=0; i < arr.length; i++){
+        arr[i]<arr[i+1]? sum-=arr[i]: sum+=arr[i]
+    }
+    return sum;
+}
+console.log(solution('MMXXVII'))
