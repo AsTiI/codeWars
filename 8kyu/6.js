5c59efd3cf2f32efaa45470aebbdeabf412a41e8
AsTiL <AsTiL@DESKTOP-M9P1BV5>
2022-04-27 12:42:41 +0300

    feat: create sameCase which check if two given characters are the same case

--- 8kyu/6.js
+++ 8kyu/6.js
@@ -3,3 +3,18 @@
 // If either of the characters is not a letter, return -1
 // If both characters are the same case, return 1
 // If both characters are letters, but not the same case, return 0
+
+function sameCase(a, b){
+    if(!/[a-zA-Z]/i.test(a) || !/[a-zA-Z]/i.test(b))
+        return -1
+    if(a === a.toLowerCase() && b === b.toLowerCase()){
+        return 1
+    }else{
+        if(a === a.toUpperCase() && b === b.toUpperCase())
+            return 1
+        else
+            return 0
+    }
+}
+
+console.log(sameCase('b', 'c'));
